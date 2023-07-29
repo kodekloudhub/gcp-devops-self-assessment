02f2f9aff4dcdd4977575e9cb2a5cc060132d44f
Snehanjan <82561283+Snehanjan2001@users.noreply.github.com>
2023-07-29 12:46:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<div style=\"text-align: center;\"><h1>Hello from KodeKloud!</h1><p>Welcome to GCP DevOps Course.</p><p></p></div>'
+    return '<div style=\"text-align: center;\"><h1>Hello from Snehanjan!</h1><p>Welcome to GCP Project.</p><p></p></div>'
 
 
 if __name__ == "__main__":
